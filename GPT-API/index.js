c2e415e711d8715f09d5d74f0dbb7b6e93df1c1c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-10 09:55:00 -0300

    Back-End e API

---
+++ GPT-API/index.js
@@ -0,0 +1,39 @@
+// part 1 que faz a requisição
+// import OpenAI from "openai";
+
+
+// const openai = new OpenAI({
+//     organization:"org-qzsU9lbW27jBN1GWIXz96VVS",
+//     apiKey:"sk-proj-ZmyFkPpxnL4DUFloEwGlT3BlbkFJbIioKdXAbEYDvaSv8bHQ"
+//     // sk-proj-ZmyFkPpxnL4DUFloEwGlT3BlbkFJbIioKdXAbEYDvaSv8bHQ
+// });
+
+// const chatCompletion = await openai.chat.completions.create({
+//     model:"gpt-3.5-turbo",
+//     messages:[{"role":"user", "content":"Quem é você?"}]
+// })
+
+// console.log(chatCompletion.choices[0].message);
+
+// Parte 2 - Criar back end completo com API local
+
+import OpenAI from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+// Criar o projeto back end a partir daqui...
+
+
+const openai = new OpenAI({
+    organization:"org-qzsU9lbW27jBN1GWIXz96VVS",
+    apiKey:"sk-proj-ZmyFkPpxnL4DUFloEwGlT3BlbkFJbIioKdXAbEYDvaSv8bHQ"
+    // sk-proj-ZmyFkPpxnL4DUFloEwGlT3BlbkFJbIioKdXAbEYDvaSv8bHQ
+});
+
+const chatCompletion = await openai.chat.completions.create({
+    model:"gpt-3.5-turbo",
+    messages:[{"role":"user", "content":"Quem é você?"}]
+})
+
+console.log(chatCompletion.choices[0].message);
